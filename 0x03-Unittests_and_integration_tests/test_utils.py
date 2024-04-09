5e9c9b6b7d5202dcb6600e4d1a6d2b7ec25786ab
Ahmedsaleh1234 <a12661826@gmail.com>
2024-04-09 02:21:17 +0200

    test

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,26 +1,46 @@
 #!/usr/bin/env python3
-""" 0. Parameterize a unit test"""
-
+"""A module for testing the utils module.
+"""
 import unittest
-from parameterized import parameterized
+from typing import Dict, Tuple, Union
 from unittest.mock import patch, Mock
-from utils import (access_nested_map, get_json, memoize)
+from parameterized import parameterized
+
+from utils import (
+    access_nested_map,
+    get_json,
+    memoize,
+)
 
 
 class TestAccessNestedMap(unittest.TestCase):
-    """ class that inherits from unittest.TestCase"""
-    @parameterized.expand([({"a": 1}, ("a",), 1),
-                           ({"a": {"b": 2}}, ("a",), {"b": 2}),
-                           ({"a": {"b": 2}}, ("a", "b"), 2)])
-    def test_access_nested_map(self, nested_map, path, exp):
-        """to test that the method returns what it is supposed to"""
-        self.assertEqual(access_nested_map(nested_map, path), exp)
+    """Tests the `access_nested_map` function."""
+    @parameterized.expand([
+        ({"a": 1}, ("a",), 1),
+        ({"a": {"b": 2}}, ("a",), {"b": 2}),
+        ({"a": {"b": 2}}, ("a", "b"), 2),
+    ])
+    def test_access_nested_map(
+            self,
+            nested_map: Dict,
+            path: Tuple[str],
+            expected: Union[Dict, int],
+    ) -> None:
+        """Tests `access_nested_map`'s output."""
+        self.assertEqual(access_nested_map(nested_map, path), expected)
 
-    @parameterized.expand([({}, ("a",), KeyError),
-                           ({"a": 1}, ("a", "b"), KeyError)])
-    def test_access_nested_map_exception(self, nested_map, path, exp):
-        """ the assertRaises context manager to test that a KeyError"""
-        with self.assertRaises(exp):
+    @parameterized.expand([
+        ({}, ("a",), KeyError),
+        ({"a": 1}, ("a", "b"), KeyError),
+    ])
+    def test_access_nested_map_exception(
+            self,
+            nested_map: Dict,
+            path: Tuple[str],
+            exception: Exception,
+    ) -> None:
+        """Tests `access_nested_map`'s exception raising."""
+        with self.assertRaises(exception):
             access_nested_map(nested_map, path)
 
 
