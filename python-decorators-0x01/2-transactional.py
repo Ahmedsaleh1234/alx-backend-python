fce1cbf24089b7e0331fdf205720e25eeeb1b94b
Ahmedsaleh1234 <a12661826@gmail.com>
2024-12-08 18:27:50 +0200

    commit

---
+++ python-decorators-0x01/2-transactional.py
@@ -0,0 +1,36 @@
+import sqlite3 
+import functools
+def with_db_connection(func):
+    """
+    handling connection to dbs
+    """
+    def wrapper(*args, **kwargs):
+        conn = sqlite3.connect('users.db')
+        try:
+            result = func(conn, *args, **kwargs)
+        finally:
+            conn.close()
+        return result
+    return wrapper
+def transactional(func):
+    def wrapper(conn, *args, **kwargs):
+        try:
+            result = func(conn, *args, **kwargs)
+            conn.commit()
+            return result
+        except Exception as e:
+            conn.rollback()
+            print(e)
+    return wrapper
+
+        
+"""your code goes here"""
+
+@with_db_connection 
+@transactional 
+def update_user_email(conn, user_id, new_email): 
+    cursor = conn.cursor() 
+    cursor.execute("UPDATE users SET email = ? WHERE user_id = ?", (new_email, user_id)) 
+#### Update user's email with automatic transaction handling 
+
+update_user_email(user_id=1, new_email='Crawford_Cartwright@hotmail.com')
