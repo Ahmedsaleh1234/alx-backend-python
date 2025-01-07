754adc6d89df759070191884e5d3ba4ca757e8f5
Ahmedsaleh1234 <a12661826@gmail.com>
2025-01-07 21:58:29 +0200

    delete

--- Django-signals_orm-0x04/messaging/signals.py
+++ Django-signals_orm-0x04/messaging/signals.py
@@ -1,6 +1,7 @@
 from django.db.models.signals import post_save, pre_save
 from django.dispatch import receiver
 from models import Notification, Message, MessageHistory
+from django.contrib.auth.models import User
 
 
 @receiver(post_save, sender=Message)
@@ -14,3 +15,9 @@ def log_message(sender, instance, **kwargs):
         old_message = Message.objects.get(pk=instance)
         if old_message.content != instance.content:
             MessageHistory.objects.create(content=instance.content, edited_by=instance.sender)
+
+@receiver(post_save, sender=User)
+def delete_related_data(sender, instance, **kwargs):
+    Message.objects.filter(sender=instance).delete()
+    Notification.objects.filter(user=instance).delete()
+    MessageHistory.objects.filter(edited_by=instance).delete()
