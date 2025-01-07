4f338f2681dc565f55dce0df25cdb4c41c8c97ff
Ahmedsaleh1234 <a12661826@gmail.com>
2025-01-07 18:28:41 +0200

    handel models

--- Django-signals_orm-0x04/messaging/models.py
+++ Django-signals_orm-0x04/messaging/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 
 class Message(models.Model):
     sender = models.ForeignKey(User, related_name='send_message', on_delete=models.CASCADE)
-    reciever = models.ForeignKey(User, related_name='recieve_message', on_delete=models.CASCADE)
+    receiver = models.ForeignKey(User, related_name='recieve_message', on_delete=models.CASCADE)
     content = models.TextField()
     timestamp = models.DateTimeField(auto_now_add=True)
     def __str__(self):
