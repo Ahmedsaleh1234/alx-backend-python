94859cca401bb87ebb443cf0ced013d46cd0c3cd
Ahmedsaleh1234 <a12661826@gmail.com>
2025-01-07 19:14:16 +0200

    message

--- Django-signals_orm-0x04/messaging_app/messaging/tests.py
+++ Django-signals_orm-0x04/messaging_app/messaging/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+from .models import Message, Notification
 
-# Create your tests here.
+class MessagingTestCase(TestCase):
+    def setUp(self):
+        self.sender = User.objects.create(username='sender')
+        self.receiver = User.objects.create(username='receiver')
+
+    def test_message_notification(self):
+        message = Message.objects.create(sender=self.sender, receiver=self.receiver, content="Hello!")
+        notification = Notification.objects.get(user=self.receiver, message=message)
+        self.assertFalse(notification.is_read)
+        self.assertEqual(notification.user, self.receiver)
