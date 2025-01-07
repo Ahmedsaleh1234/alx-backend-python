42549163f4e040f2e5bda8e576ee07b69b12beac
Ahmedsaleh1234 <a12661826@gmail.com>
2025-01-07 18:27:12 +0200

    handel models

---
+++ Django-signals_orm-0x04/messaging/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+from django.contrib.auth.models import User
+# Create your models here.
+
+class Message(models.Model):
+    sender = models.ForeignKey(User, related_name='send_message', on_delete=models.CASCADE)
+    reciever = models.ForeignKey(User, related_name='recieve_message', on_delete=models.CASCADE)
+    content = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+    def __str__(self):
+        return f'{self.sender} to {self.reciever}'
+
+class Notifaction(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    Message = models.ForeignKey(Message, on_delete=models.CASCADE)
+    is_read = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'Notifaction for {self.user}'
+
+
+
